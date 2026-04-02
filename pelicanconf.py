50f1479d8c3087b2e42d30e48a52e5ee190c3faa
Artur Barseghyan <artur.barseghyan@gmail.com>
2026-04-03 01:00:40 +0200

    Initial

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,17 +11,17 @@ DEFAULT_LANG = "en"
  # reST field lists (:date:, :category:, :tags:) are parsed automatically
 DEFAULT_DATE = "fs"
 
-ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
-ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
+ARTICLE_URL = "/gist-blog/posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "/gist-blog/posts/{date:%Y}/{date:%m}/{slug}/index.html"
 
-PAGE_URL = "pages/{slug}/"
-PAGE_SAVE_AS = "pages/{slug}/index.html"
+PAGE_URL = "/gist-blog/pages/{slug}/"
+PAGE_SAVE_AS = "/gist-blog/pages/{slug}/index.html"
 
 STATIC_PATHS = ["static"]
 
 MENUITEMS = (
-    ("Home", "/"),
-    ("Search", "/pages/search/"),
+    ("Home", "/gist-blog/"),
+    ("Search", "/gist-blog/pages/search/"),
 )
 
 SUMMARY_MAX_LENGTH = 50
