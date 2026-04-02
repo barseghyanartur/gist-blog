106c55cad9f2fc69919081d9f9544ead66bf3fbe
Artur Barseghyan <artur.barseghyan@gmail.com>
2026-04-03 01:07:04 +0200

    Initial

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,25 +3,24 @@ SITENAME = "Dev Notes"
 SITEURL = "/gist-blog"
 
 PATH = "content"
-# THEME = 'simple'                    # clean & responsive
+# THEME = "simple"
 
 TIMEZONE = "UTC"
 DEFAULT_LANG = "en"
 
- # reST field lists (:date:, :category:, :tags:) are parsed automatically
 DEFAULT_DATE = "fs"
 
-ARTICLE_URL = "/gist-blog/posts/{date:%Y}/{date:%m}/{slug}/"
-ARTICLE_SAVE_AS = "/gist-blog/posts/{date:%Y}/{date:%m}/{slug}/index.html"
+ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
 
-PAGE_URL = "/gist-blog/pages/{slug}/"
-PAGE_SAVE_AS = "/gist-blog/pages/{slug}/index.html"
+PAGE_URL = "pages/{slug}/"
+PAGE_SAVE_AS = "pages/{slug}/index.html"
 
 STATIC_PATHS = ["static"]
 
 MENUITEMS = (
-    ("Home", "/gist-blog/"),
-    ("Search", "/gist-blog/pages/search/"),
+    ("Home", "/"),
+    ("Search", "/pages/search/"),
 )
 
 SUMMARY_MAX_LENGTH = 50
